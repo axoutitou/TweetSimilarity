863f210209b6c70c55094497a67b86b47ffb9cff
fpa-alex <68065103+fpa-alex@users.noreply.github.com>
2020-12-11 00:30:39 +0100

    Create jenkins.Prod.groovy

---
+++ jenkins.Prod.groovy
@@ -0,0 +1,17 @@
+def build_app(){
+  echo 'Dev branch is building and launching'
+}
+
+def test_app(){
+  echo 'Tests are executing'
+}
+
+def down_app(){
+  echo 'Application is shutting down'
+}
+
+def release_app(){
+  echo 'Branch into release'
+}
+
+return this
