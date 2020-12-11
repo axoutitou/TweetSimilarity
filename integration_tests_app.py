0fadd9c0cd9ac39225f4f04e905a72968cb42d80
Florin-Alexandru Poschina <fpa@edifixio.com>
2020-12-11 16:01:23 +0100

    Added the test on a thousand requests

--- integration_tests_app.py
+++ integration_tests_app.py
@@ -3,6 +3,7 @@ import os
 import requests
 from flask import Flask, request, render_template
 from pyramid import testing
+import time
 
 class FlaskTests(unittest.TestCase):
 
@@ -22,6 +23,22 @@ class FlaskTests(unittest.TestCase):
 		}		
 		response = requests.post('http://localhost:5000', data=params)
 		self.assertEqual(response.status_code, 200)
+		
+	def test_1000RequestsPerMinute(self):
+		start = time.clock()
+		params = {
+			'tweet': self.parameters['phrase']
+		}
+		for i in range(1000):
+			response = requests.post('http://localhost:5000', data=params)
+			self.assertEqual(response.status_code, 200)
+				
+		request_time = time.clock() - start
+		print(request_time)
+		self.assertTrue(request_time < 60)
+		
+	
+		
 
 if __name__ == '__main__':
 	unittest.main()
