9ce7f739a2a039187e4c7ec94dfca1f3f12c1e23
fpa-alex <68065103+fpa-alex@users.noreply.github.com>
2020-12-11 01:09:48 +0100

    Update jenkins.Release.groovy

--- jenkins.Release.groovy
+++ jenkins.Release.groovy
@@ -3,8 +3,13 @@ def build_app(){
   bat 'docker-compose up -d'
 }
 
+def user_acceptance(){
+ input "proceed with deployement to Prod?" 
+}
+
 def test_app(){
   echo 'Tests are executing'
+  user_acceptance()
 }
 
 def down_app(){
