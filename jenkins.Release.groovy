f0d129944f91a642b406cf947a7abca44261ed78
fpa-alex <68065103+fpa-alex@users.noreply.github.com>
2020-12-11 16:07:33 +0100

    Update jenkins.Release.groovy

--- jenkins.Release.groovy
+++ jenkins.Release.groovy
@@ -1,6 +1,6 @@
 def build_app(){
   echo 'Dev branch is building and launching'
-  powershell 'docker-compose up -d'
+  bat 'docker-compose up -d'
 }
 
 def user_acceptance(){
@@ -14,16 +14,16 @@ def test_app(){
 
 def down_app(){
   echo 'Application is shutting down'
-  powershell 'docker-compose down'
+  bat 'docker-compose down'
 }
 
 def release_app(){
  withCredentials([usernamePassword(credentialsId: 'fpa-alex', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
     echo 'Merge into prod branch'             
-    powershell 'git remote set-url origin https://${USERNAME}:${PASSWORD}@github.com/axoutitou/TweetSimilarity.git'
-    powershell 'git checkout Prod'
-    powershell 'git merge Release'
-    powershell 'git push origin Prod'
+    bat 'git remote set-url origin https://${USERNAME}:${PASSWORD}@github.com/axoutitou/TweetSimilarity.git'
+    bat 'git checkout Prod'
+    bat 'git merge Release'
+    bat 'git push origin Prod'
   }
 }
 
