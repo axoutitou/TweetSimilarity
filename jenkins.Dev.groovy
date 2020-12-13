f2c3d92fe109ce5c2ed6c5783a921e7533607301
fpa-alex <68065103+fpa-alex@users.noreply.github.com>
2020-12-13 13:42:37 +0100

    Update jenkins.Dev.groovy

--- jenkins.Dev.groovy
+++ jenkins.Dev.groovy
@@ -5,13 +5,14 @@ def build_app(){
 
 def unit_test(){
   echo 'Unit tests are executing'
-  bat 'C:\\Users\\fpa\\AppData\\Local\\Microsoft\\WindowsApps\\python.exe test_app.py'
+  bat 'python version'
+  //bat 'C:\\Users\\fpa\\AppData\\Local\\Microsoft\\WindowsApps\\python.exe test_app.py'
 }
 
 
 def integration_test(){
   echo 'Integration tests are executing'
-  bat 'C:\\Users\\fpa\\AppData\\Local\\Microsoft\\WindowsApps\\python.exee integration_tests_app.py'
+  //bat 'C:\\Users\\fpa\\AppData\\Local\\Microsoft\\WindowsApps\\python.exee integration_tests_app.py'
 }
 
 def test_app(){
